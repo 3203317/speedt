c5f86abd788fd7511104427404ff3059bc9ac9f3
3203317 <3203317@qq.com>
2014-10-03 22:42:25 +0800

    haha

--- test/server/app.js
+++ test/server/app.js
@@ -3,6 +3,10 @@ var speedt = require('../../');
 var app = speedt.createApp();
 app.set('name', 'uplserv');
 
+app.configure('production|development', function(){
+	app.filter();
+});
+
 app.start();
 
 process.on('uncaughtException', function (err){
